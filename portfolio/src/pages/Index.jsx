6f04c61623ed58f7bd883f3a677791c2a62bb1c4
vedanti <vedanti.r.bagade@gmail.com>
2025-05-23 13:03:40 +0530

    file updated

---
+++ portfolio/src/pages/Index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Education from "../components/Education";
+import Projects from "../components/Projects";
+import Certifications from "../components/Certifications";
+import Hobbies from "../components/Hobbies";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <Hero />
+        <About />
+        <Education />
+        <Projects />
+        <Certifications />
+        <Hobbies />
+        <Contact />
+      </div>
+      <Footer />
+    </div>
+  );
+};
