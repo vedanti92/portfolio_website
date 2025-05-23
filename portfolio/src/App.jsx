dfcb6e47e8752b1b8a986b09bc1526aaa83d2e36
vedanti <vedanti.r.bagade@gmail.com>
2025-05-23 13:12:01 +0530

    file updated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,17 @@
-import './App.css'
+import "./App.css";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
