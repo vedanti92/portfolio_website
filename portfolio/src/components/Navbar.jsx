7482a673c11d7762df5f4a11b230c0df70311dae
vedanti <vedanti.r.bagade@gmail.com>
2025-05-31 23:29:44 +0530

    file updated

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Navbar = () => {
+  const navLinks = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Education", href: "#education" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+  ];
+
   return <div></div>;
 };
 
