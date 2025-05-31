e1aa662adaabe581535ac1415e7da1131effccee
vedanti <vedanti.r.bagade@gmail.com>
2025-05-31 23:33:51 +0530

    file updated

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -9,7 +9,15 @@ const Navbar = () => {
     { name: "Contact", href: "#contact" },
   ];
 
-  return <div></div>;
+  return (
+    <nav className="navbar fixed-top">
+      <div className="container d-flex justify-content-between align-items-center">
+        <a href="#home" className="navbar-brand fw-bold">
+          Portfolio.
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
