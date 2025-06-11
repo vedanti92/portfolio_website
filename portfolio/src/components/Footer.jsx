e9bf442e7087d905452350339c7f610a5e531e3a
vedanti <vedanti.r.bagade@gmail.com>
2025-06-11 19:51:13 +0530

    file updated

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
     <footer className="border-top py-5">
       <div className="footer-container d-flex flex-column flex-md-row justify-content-between align-items-center gap-3 px-5">
         <div>
-          <p>
+          <p className="text-muted">
             &copy; {new Date().getFullYear()} Vedanti Bagade. All rights
             reserved.
           </p>
@@ -21,7 +21,11 @@ const Footer = () => {
         <div>
           <button
             className="btn btn-sm rounded-circle d-flex align-items-center justify-content-center"
-            style={{ width: "36px", height: "36px" }}
+            style={{
+              width: "36px",
+              height: "36px",
+              backgroundColor: "#0e0f28",
+            }}
             onClick={scrollToTop}
           >
             <ArrowUp size={20} />
