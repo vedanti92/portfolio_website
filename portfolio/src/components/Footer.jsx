d8573641bbd755513459bc9cf92cda98cd9da756
vedanti <vedanti.r.bagade@gmail.com>
2025-06-11 19:42:18 +0530

    file updated

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { ArrowUp } from "lucide-react";
 
 const Footer = () => {
-  return <div></div>;
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <footer className="border-top py-5">
+      <div className="footer-container flex justify-between mx-auto px-4">
+        <div>
+          <p>
+            &copy; {new Date().getFullYear()} Vedanti Bagade. All rights
+            reserved.
+          </p>
+        </div>
+        <div>
+          <button className="" onClick={scrollToTop}>
+            <ArrowUp className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
