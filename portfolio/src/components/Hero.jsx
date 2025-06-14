f8c08bd2703caa6b47493126b091ed87c5e05f9e
vedanti <vedanti.r.bagade@gmail.com>
2025-06-14 17:57:44 +0530

    file updated

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -37,7 +37,15 @@ const Hero = () => {
     }, 30);
   };
 
-  return <section></section>;
+  return (
+    <section id="home">
+      {/* Glowing Circles */}
+      <div className="position-absolute w-100 h-100 top-0 start-0 z-n1">
+        <div className="glow-circle bg-primary top-25 start-25"></div>
+        <div className="glow-circle bg-accent bottom-33 end-25"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
